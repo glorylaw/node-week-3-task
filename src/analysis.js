23d61ec6f7ea32fde81be20bccfda74a943630a0
glorylaw <100371169+glorylaw@users.noreply.github.com>
2022-10-11 23:13:03 +0100

    week 3 task updated

--- src/analysis.js
+++ src/analysis.js
@@ -1,4 +1,4 @@
-const { getTrips } = require('api');
+const { getTrips,getDriver } = require('api');
 
 /**
  * This function should return the trip data analysis
@@ -8,6 +8,94 @@ const { getTrips } = require('api');
  */
 async function analysis() {
   // Your code goes here
-}
+            let tripData = await getTrips()
+          //collect all the drivers Id without duplicates(unique set)
+          const allDriversId=[...new Set(tripData.map((item)=>item.driverID))]
+          const allDrivers = {}
+          const driversInfo = new Map()
+          await Promise.allSettled(allDriversId.map((id) =>getDriver(id).then((res) => (allDrivers[id] = res))))
+//console.log(allDriversId)
 
-module.exports = analysis;
+          const output ={
+            noOfCashTrips: 0,
+            noOfNonCashTrips: 0,
+            billedTotal: 0,
+            cashBilledTotal:0,
+            nonCashBilledTotal: 0,
+            noOfDriversWithMoreThanOneVehicle: 0,
+            mostTripsByDriver: {
+              name: "",
+              email: "",
+              phone: "",
+              noOfTrips: 0,
+              totalAmountEarned: 0
+            },
+            highestEarningDriver: {
+              name: "",
+              email: "",
+              phone: "",
+              noOfTrips: 0,
+              totalAmountEarned: 0
+            }
+          }
+          for(const trip of tripData){
+            const moneyBills = Number(trip.billedAmount) || Number(trip.billedAmount.replace(",", ""))
+            if(trip.isCash===true){
+              output.noOfCashTrips++
+              output.cashBilledTotal += moneyBills
+
+            }
+            else {
+              output.noOfNonCashTrips++
+              output.nonCashBilledTotal += moneyBills
+            }
+            output.billedTotal += moneyBills
+            const driverID = trip.driverID
+            const driverDetails = driversInfo.get(driverID)
+            if(driverDetails){
+              driversInfo.set(driverID, {
+                driverID,
+                noOfTrips: driverDetails.noOfTrips + 1,
+                billedTotal: driverDetails.billedTotal + moneyBills
+              })
+            }
+            else{driversInfo.set(driverID, {
+              driverID,
+              noOfTrips: 1,
+              billedTotal: moneyBills
+            })
+            }
+
+          }
+          output.billedTotal = +output.billedTotal.toFixed(2);
+          output.nonCashBilledTotal = +output.nonCashBilledTotal.toFixed(2);
+          const values = [...driversInfo.values()]
+          const driverWithMostTrips = values.sort((a, b) => b.noOfTrips - a.noOfTrips)[0];
+          const driverWithMostEarnings = values.sort((a, b) => b.billedTotal - a.billedTotal)[0];
+          // Calculate no of drivers with more than one vehicles
+          for (const id in allDrivers) {
+              if (allDrivers[id].vehicleID.length > 1)
+                  output.noOfDriversWithMoreThanOneVehicle++;
+          }
+          if (driverWithMostTrips.driverID in allDrivers) {
+              const id = driverWithMostTrips.driverID
+              output.mostTripsByDriver.name = allDrivers[id].name
+              output.mostTripsByDriver.email = allDrivers[id].email
+              output.mostTripsByDriver.phone = allDrivers[id].phone
+              output.mostTripsByDriver.noOfTrips = driverWithMostTrips.noOfTrips
+              output.mostTripsByDriver.totalAmountEarned = +driverWithMostTrips.billedTotal
+          }
+          if (driverWithMostEarnings.driverID in allDrivers) {
+              const id = driverWithMostEarnings.driverID
+              output.highestEarningDriver.name = allDrivers[id].name
+              output.highestEarningDriver.email = allDrivers[id].email
+              output.highestEarningDriver.phone = allDrivers[id].phone
+              output.highestEarningDriver.noOfTrips = driverWithMostEarnings.noOfTrips
+              output.highestEarningDriver.totalAmountEarned = +driverWithMostEarnings.billedTotal
+          }
+          
+          return output;
+          }
+
+          module.exports = analysis;
+          analysis()
--- src/report.js
+++ src/report.js
@@ -1,4 +1,4 @@
-const { getTrips } = require('api');
+const { getTrips, getDriver, getVehicle } = require('api');
 
 /**
  * This function should return the data for drivers in the specified format
@@ -9,6 +9,109 @@ const { getTrips } = require('api');
  */
 async function driverReport() {
   // Your code goes here
-}
+  const report=[]
+  const trips = await getTrips()
+  //get drivers id
+  const ids=[...new Set(trips.map((item =>{return item.driverID})))]
+  for (let i= 0; i<ids.length;i++){
+    let driverID =ids[i]
+    try{
+      let driverObject = await getDriver(driverID)
+      if(driverObject){
+        let tripData= await getTripData(driverID,trips)
+        let vehicleData = await getVehicles(driverObject.vehicleID)
+        
+        //create a result object.
+        const resultObj = {
+          fullName: driverObject.name,
+          id: driverID,
+          phone: driverObject.phone,
+          noOfTrips:tripData.noOfTrips,
+          noOfVehicles: vehicleData.length,
+          vehicles: vehicleData,
+          noOfCashTrips: tripData.noOfCashTrips,
+          noOfNonCashTrips: tripData.noOfNonCashTrips,
+          totalAmountEarned: tripData.totalAmountEarned.toFixed(2),
+          totalCashAmount: tripData.totalCashAmount.toFixed(2),
+          totalNonCashAmount: tripData.totalNonCashAmountl.toFixed(2),
+          trips: tripData.trips,
+      
 
+        }
+        report.push(resultObj)
+      }
+    }catch(err){
+      console.log(err.message)
+    }
+
+  }
+  return report
+}
+//acquire the information of the trips by defining a function
+async function getTripData(driverID,trips){
+  let dataTripObj = {
+    noOfTrips: 0,
+    noOfCashTrips: 0,
+    noOfNonCashTrips: 0,
+    totalAmountEarned: 0,
+    totalCashAmount: 0,
+    totalNonCashAmount: 0,
+    trips: [],
+  }
+for(let i=0 ;i<trips.length;i++){
+  let tripElement=trips[i]
+  let billValueString = tripElement.billedAmount
+  if(typeof billValueString === "string" && billValueString.includes(",")){
+    billValueString =billValueString.split(",").join("")
+  }else{
+    billValueString =billValueString
+  }
+  const billValueNum = parseFloat(billValueString)
+  let cashTripData ={
+    user: tripElement.user,
+    created: tripElement.created,
+    pickup: tripElement.pickup.address,
+    destination: tripElement.destination.address,
+    billed: billValueNum,
+    isCash: true,
+  }
+  if(tripElement.driverID === driverID){
+    dataTripObj.noOfTrips +=1
+    if (tripElement.isCash ===true){
+      dataTripObj.noOfCashTrips +=1
+      dataTripObj.totalCashAmount += billValueNum
+    }
+    if (tripElement.isCash ===false){
+      dataTripObj.noOfNonCashTrips +=1
+      dataTripObj.totalNonCashAmount += billValueNum
+    }
+    dataTripObj.trips.push(cashTripData)
+  }
+}
+return  dataTripObj
+}
+// get vehicleIDs
+async function getVehicles(vehicleIDs) {
+  let vehicleData = [
+    {
+      plate: '',
+      manufacturer: '',
+    },
+  ]
+  for (let i = 0; i < vehicleIDs.length; i++) {
+    try {
+      let vehicleItem = await getVehicle(vehicleIDs[i])
+      let vehicleObj = {
+        plate: '',
+        manufacturer: '',
+      }
+      vehicleObj.plate = vehicleItem.plate
+      vehicleObj.manufacturer = vehicleItem.manufacturer
+      vehicleData.push(vehicleObj)
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+  return vehicleData
+}
 module.exports = driverReport;
